6fde7b4ba18c96a94bb46813d3cc7b8c67bdd593
Sean Turner <seanturner@outlook.com>
2024-01-06 10:13:34 -0500

    vertical monitor refresh

--- dot_config/polybar/executable_launch.sh
+++ dot_config/polybar/executable_launch.sh
@@ -2,11 +2,12 @@
 # https://wiki.archlinux.org/title/Polybar
 
 # Terminate already running bar instances
-killall -q polybar
-# If all your bars have ipc enabled, you can also use
 # polybar-msg cmd quit
+killall -q polybar
 
 # Launch Polybar, using default config location ~/.config/polybar/config.ini
-polybar main 2>&1 | tee -a /tmp/polybar.log & disown
+echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+polybar main 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar vertical 2>&1 | tee -a /tmp/polybar2.log & disown
 
 echo "Polybar launched..."
